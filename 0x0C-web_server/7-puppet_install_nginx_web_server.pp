de5d0f3546f3da77c0708ae1e0633e869577ff06
Timi Adenuga <timswaggs1@outlook.com>
2022-12-14 03:59:31 +0100

    added redirection to puppet file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,5 @@
 # using puppet, install nginx and configure it to serve a page with the content "hello world"
+# redirect using "301 removed permanently"
 
 package { 'nginx':
   ensure => 'installed',
@@ -9,8 +10,17 @@ file { '/usr/share/nginx/html/index.html':
   content => 'hello world',
 }
 
+
+file { '/etc/nginx/conf.d/redirect.conf':
+  ensure  => 'file',
+  content => 'server {
+    listen 80/redirect_me;
+    server_name github.com;
+    return 301 http://www.github.com/thi-mee;
+  }',
+}
+
 service { 'nginx':
   ensure => 'running',
   enable => true,
 }
-
